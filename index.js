6acde1ad4cba18382e0a4c2687588b24259717bf
Alan Lima <alan.silva1f@gmail.com>
2023-07-03 20:32:39 -0300

    Commitando dados novos

--- index.js
+++ index.js
@@ -5,92 +5,98 @@ const operadoresSimbols = document.querySelectorAll(".simbols");
 const clearResult = document.querySelector(".clear");
 const backDelete = document.querySelector(".backDelete");
 
-let numero1 = 0;
-let numero2 = 0;
+let numerosOperando = [0,0];
 let operador = null;
 let resultFinal = 0;
 
-const pegarSimbols = $evt =>{
-  if(numero1 === 0) return
+const pegarSimbols = ($evt) => {
+  if (numerosOperando[0] === 0) return;
   resultFinal = 0;
-  numero2 = 0;
   operador = $evt.target.textContent;
   resultvalue.value = "";
-}
+};
 
 const mapearButtons = () => {
   numOperandos.forEach((item) => {
     item.addEventListener("click", ($evt) => {
-        resultvalue.value += $evt.target.textContent;
-        
-        if(operador === null){
-          numero1 += $evt.target.textContent; 
-        }else{
-          numero2 += $evt.target.textContent; 
-        }
+      let buttonValue = $evt.target.textContent;
+      resultvalue.value += buttonValue;
+
+      if (operador === null) {
+        numerosOperando[0] += buttonValue;
+      } else {
+        numerosOperando[1] += buttonValue;
+      }
     });
   });
 };
 
-const calcularOperadores = operador => {
-    let res = 0;
+const calcularOperadores = (operador) => {
+  let res = 0;
 
-    switch (operador) {
-        case '+':
-          res = Number.parseFloat(numero1) + Number.parseFloat(numero2);
-          break;
-        case '-':
-          res = Number.parseFloat(numero1) - Number.parseFloat(numero2);
-          break;
-        case '*':
-          res = Number.parseFloat(numero1) * Number.parseFloat(numero2);
-          break;
-        case '/':
-          res = Number.parseFloat(numero1) / Number.parseFloat(numero2);
-          break;
-        default:
-          alert("Resultado Invalido....");
-          break;
-      }
- 
-    return res;
+  const [numero1,numero2] = numerosOperando;
+  
+  switch (operador) {
+    case "+":
+      res = Number.parseFloat(numero1) + Number.parseFloat(numero2);
+      break;
+    case "-":
+      res = Number.parseFloat(numero1) - Number.parseFloat(numero2);
+      break;
+    case "*":
+      res = Number.parseFloat(numero1) * Number.parseFloat(numero2);
+      break;
+    case "/":
+      res = Number.parseFloat(numero1) / Number.parseFloat(numero2);
+      break;
+    default:
+      alert("Resultado Invalido....");
+      break;
+  }
+
+  return res;
 };
 
-const resultCalc = result => {
-  numero1 = result;
+const resultCalc = (result) => {
+  numerosOperando[0] = result;
   resultFinal = result;
   resultvalue.value = result;
 };
 
-btnVerificarResult.addEventListener("click", () =>{
-  if(numero2 === 0) return
-    const calRes = calcularOperadores(operador);
-    const numberFormatResult = calRes.toLocaleString('pt-BR', { currency: 'BRL' });
-    resultCalc(numberFormatResult)
+btnVerificarResult.addEventListener("click", () => {
+  if (numerosOperando[1] === 0) return;
+  const calRes = calcularOperadores(operador);
+  const numberFormatResult = calRes.toLocaleString("pt-BR", {
+    currency: "BRL",
+  });
+
+  resultCalc(numberFormatResult);
+  numerosOperando[1]= 0;
 });
 
-const zeraResultado = () =>{
-  numero1 = 0;
-  numero2 = 0;
+const zeraResultado = () => {
+  numerosOperando[0] = 0;
+  numerosOperando[1] = 0;
   operador = null;
   resultFinal = 0;
   resultvalue.value = "";
-}
+};
 
-const deleteNum = () =>{
-  if(resultFinal !== 0) return
-  
+const deleteNum = () => {
+  if (resultFinal !== 0) return;
+
   const deleteArr = resultvalue.value.split("");
-  let alan = deleteArr.splice(0,deleteArr.length -1).join("");
+  let alan = deleteArr.splice(0, deleteArr.length - 1).join("");
 
-  operador === null ? numero1 = alan : numero2 = alan 
+  operador === null ? (numerosOperando[0] = alan) : (numerosOperando[1] = alan);
 
-  resultvalue.value = alan
-}
+  resultvalue.value = alan;
+};
 
 clearResult.addEventListener("click", zeraResultado);
-operadoresSimbols.forEach(item => item.addEventListener("click", pegarSimbols));
+operadoresSimbols.forEach((item) =>
+  item.addEventListener("click", pegarSimbols)
+);
+
 backDelete.addEventListener("click", deleteNum);
 mapearButtons();
-
-
